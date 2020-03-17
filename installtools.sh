8986f759a20f09150be3cec465c9927cddd8d735
Tom Elliott <telliott@yext.com>
2020-03-17 18:07:50 -0400

    Comment out items that don't work.

--- installtools.sh
+++ installtools.sh
@@ -9,7 +9,10 @@ go install github.com/theothertomelliott/go-module-versioning
 go install github.com/gogo/protobuf/protoc-gen-gogofast
 go install github.com/golang/protobuf/protoc-gen-go
 go install github.com/google/wire/cmd/wire
-go install github.com/googlecodelabs/tools/claat
+
+# The current version does not build due to blackfriday compatibility issues
+# go install github.com/googlecodelabs/tools/claat
+
 go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
 go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
 go install github.com/josharian/impl
@@ -17,7 +20,10 @@ go install github.com/kardianos/govendor
 go install github.com/kisielk/errcheck
 go install github.com/mailru/easyjson/easyjson
 go install github.com/mdempsky/gocode
-go install github.com/prometheus/prometheus/cmd/promtool
+
+# There are issues with promtool as a result of github.com/Azure/azure-sdk-for-go
+#go install github.com/prometheus/prometheus/cmd/promtool
+
 go install github.com/robfig/glock
 go install github.com/yext/revel/revel
 go install github.com/zmb3/gogetdoc
